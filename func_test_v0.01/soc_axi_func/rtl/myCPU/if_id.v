4da57cbe441a33871c7497d1089d7aabf61e1775
BlitherBoom812 <tsz21@mails.tsinghua.edu.cn>
2023-07-31 22:20:10 +0800

    revise pc & if_id

--- func_test_v0.01/soc_axi_func/rtl/myCPU/if_id.v
+++ func_test_v0.01/soc_axi_func/rtl/myCPU/if_id.v
@@ -1,28 +1,29 @@
 `include "defines.vh"
-module if_id(
-    input                    rst,
-    input                    clk,
-    input  [31:0]            if_pc,
-    input  [31:0]            if_instr,
-    input                    exception,
-    input  [3:0]             stall,
-	input  [31:0]            if_exception_type,
-		
-	output reg [31:0]        id_exception_type,
-    output reg [31:0]        id_pc,
-    output reg [31:0]        id_instr
+
+module if_id (
+    input        reset_i,
+    input        clock_i,
+    input [31:0] if_pc_i,
+    input [31:0] if_inst_i,
+    input        exception_i,
+    input [ 3:0] stall_i,
+    input [31:0] if_exception_type_i,
+
+    output reg [31:0] id_exception_type_o,
+    output reg [31:0] id_pc_o,
+    output reg [31:0] id_inst_o
 );
 
-always @ (posedge clk) begin
-    if (rst == 1'b0 || exception == 1'b1) begin
-        id_pc <= 32'b0;
-        id_instr <= 32'b0;
-        id_exception_type <= 6'h0;
-    end 
-    else if (stall == 4'b0000)begin
-        id_pc <= if_pc;
-        id_instr <= if_instr;
-        id_exception_type <= if_exception_type;
+    always @(posedge clock_i) begin
+        if (reset_i == 1'b0 || exception_i == 1'b1) begin
+            id_pc_o             <= 32'b0;
+            id_inst_o          <= 32'b0;
+            id_exception_type_o <= 6'h0;
+        end else if (stall_i == 4'b0000) begin
+            id_pc_o             <= if_pc_i;
+            id_inst_o          <= if_inst_i;
+            id_exception_type_o <= if_exception_type_i;
+        end
     end
-end
+
 endmodule
--- func_test_v0.01/soc_axi_func/rtl/myCPU/pc.v
+++ func_test_v0.01/soc_axi_func/rtl/myCPU/pc.v
@@ -1,21 +1,37 @@
 `include "defines.vh"
-module pc(
-    input                     rst,
-    input                     clk,
-    input [3:0]               stall,
-    input                     exception,
-    input [31:0]              exception_pc_i,
-    input                     branch_enable_i,
-    input [31:0]              branch_addr_i,
 
-    output [31:0]             exception_type_o,
-    output reg[31:0]          pc_o
+module pc (
+    input        reset_i,
+    input        clock_i,
+    input [ 3:0] stall_i,
+    input        exception_i,
+    input [31:0] exception_pc_i,
+    input        branch_enable_i,
+    input [31:0] branch_addr_i,
+
+    output     [31:0] exception_type_o,
+    output reg [31:0] pc_o
 );
 
-always @ (posedge clk) begin
-    if (rst == 1'b0) pc_o <= 32'hbfc0_0000;
-    else if (exception == 1) pc_o <= exception_pc_i;
-    else if(stall==4'b0000) pc_o <= (branch_enable_i == 1) ? branch_addr_i : pc_o + 4;
-end
-assign exception_type_o = (pc_o[1:0] == 2'b00) ? {32'b0} :  {1'b1,31'b0};
+    wire [31:0] pc_next;
+
+    always @(posedge clock_i) begin
+        if (reset_i == 1'b0) pc_o <= 32'hbfc0_0000;
+        else pc_o <= pc_next;
+    end
+
+    assign pc_next = 
+        (exception_i === 1'b1) ?
+            exception_pc_i 
+        :
+            (stall_i === 4'b0000) ? 
+                (branch_enable_i === 1'b1) ? 
+                    branch_addr_i 
+                : 
+                    pc_o + 4 
+            : 
+                pc_o;
+
+    assign exception_type_o = (pc_o[1:0] === 2'b00) ? {32'b0} : {1'b1, 31'b0};
+
 endmodule
